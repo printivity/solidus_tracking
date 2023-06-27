8198a7c83bf6290fe95cc298e7b8737d55b6e145
Adnan Abdulally <adnan.abdulally@printivity.com>
2023-06-27 12:34:25 -0700

    fix decorator syntax

--- app/decorators/subscribers/solidus_tracking/spree/mailer_subscriber/disable_confirm_email.rb
+++ app/decorators/subscribers/solidus_tracking/spree/mailer_subscriber/disable_confirm_email.rb
@@ -5,15 +5,13 @@ module SolidusTracking
     module MailerSubscriber
       module DisableConfirmEmail
         def self.prepended(base)
-          base.module_eval do
-            alias_method :original_order_finalized, :order_finalized
+          base.alias_method :original_order_finalized, :order_finalized
+        end
 
-            def order_finalized(event)
-              return if SolidusTracking.configuration.disable_builtin_emails
+        def order_finalized(event)
+          return if SolidusTracking.configuration.disable_builtin_emails
 
-              original_order_finalized(event)
-            end
-          end
+          original_order_finalized(event)
         end
       end
     end
